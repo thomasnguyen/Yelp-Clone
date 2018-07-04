b9deeb7050ee4764e968dedc8953f214c36f3c94
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-03 22:58:17 -0700

    basic concept of routing

--- readme.md
+++ readme.md
@@ -25,3 +25,4 @@ That will populate 16 stores with 3 authors and 41 reviews. The logins for the a
 |Beau|beau@example.com|beau|
 
 
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const thomas = {name:'Thomas', age: 25, cool:true};
+  //res.send('Hey! It works! DUNDUN DUN');
+  //res.json(thomas);
+  //res.send(req.query.name);
+  res.send(req.query);
+});
+
+router.get('/reverse/:name',(req,res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
