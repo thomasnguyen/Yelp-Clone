6602323c10ed1fc16b550c41fcfe69d4f5b1c5ed
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-06 21:22:52 -0700

    Got a good understanding of middle and error handling

--- controllers/StoreController.js
+++ controllers/StoreController.js
@@ -1,3 +1,14 @@
+exports.myMiddlware = (req, res, next) => {
+	req.name = 'Wes';
+	res.cookie('name', 'wes is cool', { maxAge: 900000 });
+	if (req.name === 'Wes') {
+		throw Error('That is a stupid name');
+	}
+
+	next(); // passes to the next parameters
+};
+
 exports.homePage = (req, res) => {
+	console.log(req.name);
 	res.render('index');
 };
