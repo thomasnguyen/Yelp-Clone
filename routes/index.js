1889eb642f9e7b5bbcd2ec6d3f10d1bee3a0225f
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-06 19:27:40 -0700

    Got a good understanding pug templating

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  const thomas = {name:'Thomas', age: 25, cool:true};
-  //res.send('Hey! It works! DUNDUN DUN');
-  //res.json(thomas);
-  //res.send(req.query.name);
-  res.send(req.query);
+	const thomas = { name: 'Thomas', age: 25, cool: true };
+	//res.send('Hey! It works! DUNDUN DUN');
+	//res.json(thomas);
+	//res.send(req.query.name);
+	//res.send(req.query);
+
+	res.render('hello', {
+		name: 'wes',
+		dog: req.query.dog
+	});
 });
 
-router.get('/reverse/:name',(req,res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
+router.get('/reverse/:name', (req, res) => {
+	const reverse = [ ...req.params.name ].reverse().join('');
+	res.send(reverse);
 });
 
 module.exports = router;
