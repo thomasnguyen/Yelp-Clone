99eadad6f6e7916612f550457ce25c3eddf0a6f6
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-29 14:18:15 -0700

    Added new view component for addStore

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const storeController = require('../controllers/StoreController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
 
 module.exports = router;
