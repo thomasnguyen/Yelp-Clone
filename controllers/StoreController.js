e86ca633de2cbb6fe6a378da8c197a47c98483aa
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-30 23:53:18 -0700

    configured flash messages

--- controllers/StoreController.js
+++ controllers/StoreController.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
+	req.flash('error', 'Something happened');
+	req.flash('success', 'Something happened');
+	req.flash('info', 'Something happened');
+	req.flash('warning', 'Something happened');
 	res.render('index');
 };
 
@@ -10,8 +14,7 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
-	await store.save();
-	console.log('it worked!');
-	res.redirect('/');
+	const store = await new Store(req.body).save();
+	req.flash('success', `Successffuly created ${store.name}. Care to leave a review`);
+	res.redirect(`/store/${store.slug}`);
 };
