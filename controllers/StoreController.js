537960a2c28bc45ed4b59d1a01b54e85d6c597f3
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-31 00:28:47 -0700

    Querying our database for stores

--- controllers/StoreController.js
+++ controllers/StoreController.js
@@ -15,6 +15,13 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
 	const store = await new Store(req.body).save();
-	req.flash('success', `Successffuly created ${store.name}. Care to leave a review`);
+	req.flash('success', `Successfuly created ${store.name}. Care to leave a review`);
 	res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+	// 1. Query the database of list of stores
+	const stores = await Store.find();
+	console.log(stores);
+	res.render('stores', { title: 'Stores', stores });
 };
