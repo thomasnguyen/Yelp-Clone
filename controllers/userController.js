455045f29c414da2ad25b8dbb652853c7cff2ee7
Thomas Nguyen <imtoshiru@gmail.com>
2018-08-11 16:46:53 -0700

    Created user account feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,3 +3,28 @@ const mongoose = require('mongoose');
 exports.loginForm = (req, res) => {
 	res.render('login', { title: 'login' });
 };
+
+exports.registerForm = (req, res) => {
+	res.render('register', { title: 'Register' });
+};
+
+exports.validateRegister = (req, res, next) => {
+	req.sanitizeBody('name');
+	req.checkBody('name', 'You must supply name').notEmpty();
+	req.checkBody('email', 'That email is not valid').isEmail();
+	req.sanitizeBody('email').normalizeEmail({
+		remove_dots: false,
+		remove_extension: false,
+		gmail_remove_subaddress: false
+	});
+	req.checkBody('password', 'Password can not be empty').isEmail();
+	req.checkBody('passwordConfirm', 'Password Confirm can not be empty').isEmail();
+	req.checkBody('passwordConfirm', 'Passwords are not the same').isEqual(req.body.password);
+
+	const errors = req.validationErrors();
+	if (errors) {
+		req.flash('error', errors.map((err) => err.msg));
+		res.render('register', { title: 'Register', body: body, flashes: req.flash() });
+	} else {
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,7 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
 
+router.get('/register', catchErrors(userController.registerForm));
+router.post('/register', userController.validateRegister);
+
 module.exports = router;
